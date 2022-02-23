b8060c342a50d4802ade76edf484a91f07209450
Jeevan <jeevan.suvarna@cloverbaytechnologies.com>
2022-02-23 17:05:15 +0530

    intro

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -18,41 +18,8 @@ export const Intro = () => {
       </ul>
       <Link
         to={{
-          pathname:
-            "import React from "react";
-import "../Styles/intro.css";
-import { Link } from "react-router-dom";
-export const Intro = () => {
-  return (
-    <div className="wrapper">
-      <div className="static-txt">Hi,</div>
-      <ul className="dynamic-txt">
-        <li>
-          <span>
-            I'm &nbsp;<p>J</p>eevan &nbsp;<p>S</p>uvarna,
-          </span>
-          <span className="des">
-            <br></br>
-            <p>W</p>eb&nbsp;<p>D</p>eveloper.
-          </span>
-        </li>
-      </ul>
-      <Link
-        to={{
-          pathname:
-            "https://drive.google.com/file/d/1Qs0DVfS2yxIh1GIL9x4sT8vjQFLc6nU1/view?usp=sharing",
-        }}
-        target="_blank"
-      >
-        {" "}
-        <button>
-          <center>Resume</center>
-        </button>
-      </Link>
-    </div>
-  );
-};
-",
+          pathname: "https://drive.google.com/file/d/1Qs0DVfS2yxIh1GIL9x4sT8vjQFLc6nU1/view?usp=sharing",
+
         }}
         target="_blank"
       >
